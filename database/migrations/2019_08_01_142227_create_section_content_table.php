44fc84b8ad9aed80cb08ef47390755aff95517aa
Erik <khanoverik@gmail.com>
2019-08-01 19:33:24 +0500

    created mogrations

---
+++ database/migrations/2019_08_01_142227_create_section_content_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSectionContentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('section_content', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('s_id');
+            $table->json('titles');
+            $table->string('slogan');
+            $table->string('caption');
+            $table->string('background', 255);
+            $table->json('slides');
+            $table->string('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('section_content');
+    }
+}
