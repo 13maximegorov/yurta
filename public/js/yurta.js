5dd606e83367282df4cb261c57afb6b9f23386e1
Erik <khanoverik@gmail.com>
2019-08-13 23:30:18 +0500

    new-2

--- public/js/yurta.js
+++ public/js/yurta.js
@@ -1,15 +1,19 @@
 let header = document.querySelector('.header');
+let navbar = document.querySelector('.navbar-brand img');
 
 if (parseInt(window.pageYOffset) > 50) {
     header.classList.add('header__active');
+    navbar.src = "img/yurta-black-logo.png";
 } 
 
 window.onscroll = (e) => {
     if (parseInt(window.pageYOffset) > 150) { 
         header.classList.add('header__active');
+        navbar.src = "img/yurta-black-logo.png";
     }
     if (parseInt(window.pageYOffset) < 50) {
         header.classList.remove('header__active');
+        navbar.src = "img/yurta-logo.png";
     }
 }
 
